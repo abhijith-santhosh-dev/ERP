ff27d86473a9fec0a962e53d1f8c7daf9682cd76
abhijith-santhosh-dev <abhijithpanickeriss@gmail.com>
2025-03-12 12:44:31 +0530

    Basic  web route for dashboard ,users,designations

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,39 @@
 <?php
 
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DesignationController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('designations', DesignationController::class)->except(['show', 'destroy']);
+    Route::resource('users', UserController::class)->except(['show', 'destroy']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
